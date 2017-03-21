00255b50878bd239219bfbcc905068aae6134b17
attapun-an <attapunanivat@gmail.com>
2017-03-21 08:48:51 +0700

    Learning Class
    Mr. T showing us how to use classes in python

---
+++ main.py
@@ -0,0 +1,9 @@
+def change_password():
+    passChecker = True
+    while passChecker:
+        newPassword = input("Input your new password: ")
+
+
+
+def length_check(newPassword):
+    if len(newPassword)
---
+++ test.py
@@ -0,0 +1,29 @@
+class user:
+    username = ""
+    password = ""
+
+userList = []
+
+"""
+for i in range(3):
+    newUser = user()
+    newUser.username = input("input username: ")
+    newUser.passWord = input("input password: ")
+    userList.append(newUser)
+"""
+
+newUser = user()
+newUser.username = "bob"
+newUser.password = "bobisawesome"
+userList.append(newUser)
+
+print(userList[0].username)
+print(userList[0].password)
+
+"""
+print(userList[1].username)
+print(userList[1].password)
+
+print(userList[2].username)
+print(userList[2].password)
+"""
